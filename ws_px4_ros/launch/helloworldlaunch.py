0d99f7c84f614910d48f015ffce82be096f6e812
sidk524 <siddharthkambli6@gmail.com>
2026-05-17 14:39:32 +0100

    basic trajectory setpoint working

--- ws_px4_ros/launch/helloworldlaunch.py
+++ ws_px4_ros/launch/helloworldlaunch.py
@@ -3,9 +3,16 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        Node(
+            package='ros_gz_bridge',
+            namespace='bridge',
+            executable="parameter_bridge",
+            arguments = ["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"]
+        ),
         Node( 
             package='test_node',
-            executable='TestNode'
+            namespace='example',
+            executable='test_node'
         )
     ])
 
--- ws_px4_ros/src/test_node/src/helloworld.cpp
+++ ws_px4_ros/src/test_node/src/helloworld.cpp
@@ -3,6 +3,7 @@
 #include <px4_msgs/msg/trajectory_setpoint.hpp>
 #include <px4_msgs/msg/vehicle_control_mode.hpp>
 #include <px4_msgs/srv/vehicle_command.hpp>
+#include <rclcpp/create_client.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/utilities.hpp>
 #include <stdint.h>
@@ -18,9 +19,24 @@ public:
     TestNode(string px4_namespace):
         Node("TestNode"),
         currState{State::init},
+        service_done{false},
+        service_result{0},
+        numSteps{0},
         offboardPublisher{this->create_publisher<OffboardControlMode>(px4_namespace + "in/offboard_control_mode", 10)},
-        trajectoryPublisher{this->create_publisher<TrajectorySetpoint>(px4_namespace + "in/trajectory_setpoint", 10)}
+        trajectoryPublisher{this->create_publisher<TrajectorySetpoint>(px4_namespace + "in/trajectory_setpoint", 10)},
+        vehicleCommandClient{this->create_client<px4_msgs::srv::VehicleCommand>(px4_namespace + "vehicle_command")}
         {
+            RCLCPP_INFO(this->get_logger(), "Starting Offboard Control example with PX4 services");
+            RCLCPP_INFO_STREAM(this->get_logger(), "Waiting for " << px4_namespace << "vehicle_command service");
+            while (!vehicleCommandClient->wait_for_service(1s)) {
+                if (!rclcpp::ok()) {
+                    RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+                    return;
+                }
+                RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
+            }
+            RCLCPP_INFO(this->get_logger(), "starting timer callback");
+
             timer = this->create_wall_timer(100ms, std::bind(&TestNode::timer_callback, this));
         }
 
@@ -38,7 +54,7 @@ private:
     State currState;
     bool service_done;
     uint8_t service_result;
-    int numSteps = 0;
+    int numSteps;
     rclcpp::Publisher<OffboardControlMode>::SharedPtr offboardPublisher;
     rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectoryPublisher;
     rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedPtr vehicleCommandClient;
@@ -54,6 +70,7 @@ private:
 };
 
 void TestNode::request_vehicle_command(uint16_t command, float param1, float param2){
+
     auto request = std::make_shared<px4_msgs::srv::VehicleCommand::Request>();
     VehicleCommand msg{};
     msg.param1 = param1;
@@ -72,8 +89,11 @@ void TestNode::request_vehicle_command(uint16_t command, float param1, float par
 };
 
 void TestNode::client_response(rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future){
+    RCLCPP_INFO(this->get_logger(), "received client response");
+
     auto status = future.wait_for(1s);
     if (status == std::future_status::ready){
+
         auto result = future.get()->reply;
         service_result = result.result;
         service_done = true;
@@ -81,10 +101,13 @@ void TestNode::client_response(rclcpp::Client<px4_msgs::srv::VehicleCommand>::Sh
 }
 
 void TestNode::switch_to_offboard_mode(){
+    RCLCPP_INFO(this->get_logger(), "requesting switch to offboard mode");
+
     request_vehicle_command(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
 };
 
 void TestNode::publish_offboard_mode(){
+
     px4_msgs::msg::OffboardControlMode msg {};
     msg.position = true;
     msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
@@ -96,24 +119,29 @@ void TestNode::publish_offboard_mode(){
 void TestNode::publish_trajectory_setpoint(){
     px4_msgs::msg::TrajectorySetpoint msg {};
     msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    msg.position = {5.0, 0.0, -5.0};
+    msg.position = {-5.0, 0.0, -5.0};
     trajectoryPublisher->publish(msg);
          
 }
 
 void TestNode::arm(){
+                RCLCPP_INFO(this->get_logger(), "requesting arm");
+
     request_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
 }
 
 
 void TestNode::disarm(){
+                    RCLCPP_INFO(this->get_logger(), "requesting disarm");
+
     request_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
 }
 
 void TestNode::timer_callback(void){
-
+    
     publish_offboard_mode();
     publish_trajectory_setpoint();
+
     
     switch(currState){
         case State::init:
@@ -124,6 +152,8 @@ void TestNode::timer_callback(void){
                 if (service_result == 0){
                     currState = State::wait_for_stable_offboard_mode;
                 } else {
+                    RCLCPP_INFO(this->get_logger(), "shutting down");
+
                     rclcpp::shutdown();
                 }
             }
@@ -139,6 +169,8 @@ void TestNode::timer_callback(void){
                 if (service_result == 0){
                     currState = State::armed;
                 } else{
+                    RCLCPP_INFO(this->get_logger(), "shutting down");
+
                     rclcpp::shutdown();
                 }
             }
@@ -152,7 +184,6 @@ void TestNode::timer_callback(void){
 
 int main(int argc, char *argv[])
 {
-	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 	rclcpp::init(argc, argv);
 	rclcpp::spin(std::make_shared<TestNode>("/fmu/"));
 	rclcpp::shutdown();
