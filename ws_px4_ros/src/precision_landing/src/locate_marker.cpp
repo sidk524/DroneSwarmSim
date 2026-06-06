698ade8870c2d8d375e7e5fdc76f5d2823e2b822
sidk524 <siddharthkambli6@gmail.com>
2026-06-06 19:28:28 +0100

    locate marker opencv plumbign

---
+++ ws_px4_ros/src/precision_landing/src/locate_marker.cpp
@@ -0,0 +1,11 @@
+#include <locate_marker.hpp>
+#include "rclcpp/rclcpp.hpp"
+#include <px4_ros2/components/mode.hpp>
+#include <px4_ros2/components/node_with_mode.hpp>
+#include <px4_ros2/components/mode_executor.hpp>
+#include <px4_ros2/control/setpoint_types/experimental/rates.hpp>
+#include <px4_ros2/control/setpoint_types/experimental/trajectory.hpp>
+
+int main(int argc, char *argv[]){
+    return 0;
+}
