0fb06cb868fd978a1d7510c7b932ea7b784dfc97
sidk524 <siddharthkambli6@gmail.com>
2026-06-10 17:07:26 +0100

    marker detection workign

--- ws_px4_ros/src/precision_landing/src/locate_marker.cpp
+++ ws_px4_ros/src/precision_landing/src/locate_marker.cpp
@@ -8,17 +8,18 @@
 #include <px4_ros2/control/setpoint_types/experimental/rates.hpp>
 #include <px4_ros2/control/setpoint_types/experimental/trajectory.hpp>
 
-
 void LocateArucoMarkerMode::image_callback(sensor_msgs::msg::Image::SharedPtr image_msg) {
     cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
-    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
     cv::aruco::ArucoDetector detector(dictionary, detectorParams);
+
     cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
     detector.detectMarkers(cv_ptr->image, markerCorners, markerIds, rejectedCandidates);
     if (markerIds.size() > 0){
         RCLCPP_DEBUG(_node.get_logger(), "marker detected");
     }
 }
+
 
 // int main(int argc, char* argv[]){
 //     using locateMarkerNode = px4_ros2::NodeWithMode<LocateArucoMarkerMode>;
